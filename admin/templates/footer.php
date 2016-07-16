59181f6c771f48d66367cf62b91fdff3e654ba4e
skyisnotblue <skyisnotblue.unpam@gmail.com>
2016-07-16 23:35:34 +0700

    Commit awal

---
+++ admin/templates/footer.php
@@ -0,0 +1,13 @@
+<section class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center wise">
+        <strong class="text-success">"If you only read the books that everyone else is reading, you can only think what everyone else is thinking."</strong><br>
+        <strong>–Haruki Murakami</strong>
+      </div>
+      <div class="col-md-12 text-center">
+        Sky is not Blue &copy; <?php echo date('Y'); ?>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ configs/databases.php
@@ -0,0 +1,7 @@
+<?php
+  return array(
+    'NAME' => 'publikasi_karya_ilmiah',
+    'USERNAME' => 'root',
+    'PASSWORD' => '',
+    'HOST' => 'localhost',
+  );
